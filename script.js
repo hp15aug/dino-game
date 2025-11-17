e098c7d846d0ebf540d0117fcdee25266628e97d
Ujjwal Bajpai <ujjwalbajpai.ec23@rvce.edu.in>
2025-11-17 22:15:53 +0530

    test

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
 const SPEED_SCALE_INCREASE = 0.00001
 
+//abc
+
 const worldElem = document.querySelector("[data-world]")
 const scoreElem = document.querySelector("[data-score]")
 const startScreenElem = document.querySelector("[data-start-screen]")
